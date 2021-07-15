88706dc97d7b23f58547f3bfad49348f108c80e0
HPROGRAM <wjs2096@gmail.com>
2021-07-15 19:31:00 +0900

    Lv1:음양더하기 문제풀이

---
+++ Lv1/음양더하기/음양더하기.js
@@ -0,0 +1,14 @@
+function solution(absolutes, signs) {
+  let sum = 0;
+  // 배열의 길이만큼 반복
+  for (let i = 0; i < absolutes.length; i++) {
+    // signs[i]가 true면 양의 정수 false면 음의정수
+    if (signs[i] === true) {
+      // sum에 반복해서 더해줌
+      sum = sum + absolutes[i];
+    } else {
+      sum = sum + -absolutes[i];
+    }
+  }
+  return sum; // sum 반환
+}
