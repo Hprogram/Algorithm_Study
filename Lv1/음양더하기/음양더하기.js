fe7311fef089ad33b9f42aae5c2384ce90f84a1a
HPROGRAM <wjs2096@gmail.com>
2021-07-18 23:24:13 +0900

    lv1:음양더하기 커밋수정

--- Lv1/음양더하기/음양더하기.js
+++ Lv1/음양더하기/음양더하기.js
@@ -5,9 +5,9 @@ function solution(absolutes, signs) {
     // signs[i]가 true면 양의 정수 false면 음의정수
     if (signs[i] === true) {
       // sum에 반복해서 더해줌
-      sum = sum + absolutes[i];
+      sum = sum + absolutes[i]; // 양의 정수
     } else {
-      sum = sum + -absolutes[i];
+      sum = sum + -absolutes[i]; // 음의 정수
     }
   }
   return sum; // sum 반환
