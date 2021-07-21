38781bb51c51b5bf04e8639f98af74c73e7c87b2
HPROGRAM <wjs2096@gmail.com>
2021-07-21 22:49:09 +0900

    lv1: 시저암호 문제풀이

---
+++ Lv1/시저암호/시저암호.js
@@ -0,0 +1,27 @@
+function solution(s, n) {
+    var answer = '';
+    
+    let smallAlpha = 'abcdefghijklmnopqrstuvwxyz'; // 소문자 나열
+    let bigAlpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // 대문자 나열
+
+    for (let i = 0; i < s.length; i++) {
+        // 공백은 그대로
+        if (s[i] === ' ') { 
+        answer = answer + ' ';
+        // 소문자일 경우
+        }else if(smallAlpha.includes(s[i])){    // 소문자에 있는지 찾음
+            let asis = smallAlpha.indexOf(s[i]);   // 몇번 인덱스에 있는지 체크
+            //소문자의 길이만큼으로 나눈 나머지는 결국 해당 알파벳의 인덱스가 됌.
+            let tobe = (asis + n + smallAlpha.length) % smallAlpha.length;             
+            answer = answer + smallAlpha[tobe]
+        // 대문자일 경우    
+        }else{                                  
+            let asis1 = bigAlpha.indexOf(s[i]);
+            let tobe1 = (asis1 + n + bigAlpha.length) % bigAlpha.length;
+            answer = answer + bigAlpha[tobe1];
+
+        }
+    } 
+    
+    return answer;
+}
