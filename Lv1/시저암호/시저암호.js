1b2128fd6644e9b67568dded3a0b0bc013c1f6b9
HPROGRAM <wjs2096@gmail.com>
2021-07-22 23:08:26 +0900

    lv1:시저암호 주석 추가

--- Lv1/시저암호/시저암호.js
+++ Lv1/시저암호/시저암호.js
@@ -13,8 +13,8 @@ function solution(s, n) {
             let asis = smallAlpha.indexOf(s[i]);   // 몇번 인덱스에 있는지 체크
             //소문자의 길이만큼으로 나눈 나머지는 결국 해당 알파벳의 인덱스가 됌.
             let tobe = (asis + n + smallAlpha.length) % smallAlpha.length;             
-            answer = answer + smallAlpha[tobe]
-        // 대문자일 경우    
+            answer = answer + smallAlpha[tobe] // 문자열에 해당 알파벳을 하나씩 추가
+        // 대문자일 경우도 마찬가지로 진행한다
         }else{                                  
             let asis1 = bigAlpha.indexOf(s[i]);
             let tobe1 = (asis1 + n + bigAlpha.length) % bigAlpha.length;
