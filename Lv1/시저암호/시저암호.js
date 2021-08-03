7db2ce0b535659a2ab003e4eb3a34f1f6381afda
HPROGRAM <wjs2096@gmail.com>
2021-08-04 03:20:24 +0900

    lv1: 시저암호 변수 변경

--- Lv1/시저암호/시저암호.js
+++ Lv1/시저암호/시저암호.js
@@ -1,27 +1,27 @@
 function solution(s, n) {
-    var answer = '';
-    
-    let smallAlpha = 'abcdefghijklmnopqrstuvwxyz'; // 소문자 나열
-    let bigAlpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // 대문자 나열
+  let answer = "";
 
-    for (let i = 0; i < s.length; i++) {
-        // 공백은 그대로
-        if (s[i] === ' ') { 
-        answer = answer + ' ';
-        // 소문자일 경우
-        }else if(smallAlpha.includes(s[i])){    // 소문자에 있는지 찾음
-            let asis = smallAlpha.indexOf(s[i]);   // 몇번 인덱스에 있는지 체크
-            //소문자의 길이만큼으로 나눈 나머지는 결국 해당 알파벳의 인덱스가 됌.
-            let tobe = (asis + n + smallAlpha.length) % smallAlpha.length;             
-            answer = answer + smallAlpha[tobe] // 문자열에 해당 알파벳을 하나씩 추가
-        // 대문자일 경우도 마찬가지로 진행한다
-        }else{                                  
-            let asis1 = bigAlpha.indexOf(s[i]);
-            let tobe1 = (asis1 + n + bigAlpha.length) % bigAlpha.length;
-            answer = answer + bigAlpha[tobe1];
+  let smallAlpha = "abcdefghijklmnopqrstuvwxyz"; // 소문자 나열
+  let bigAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // 대문자 나열
 
-        }
-    } 
-    
-    return answer;
+  for (let i = 0; i < s.length; i++) {
+    // 공백은 그대로
+    if (s[i] === " ") {
+      answer = answer + " ";
+      // 소문자일 경우
+    } else if (smallAlpha.includes(s[i])) {
+      // 소문자에 있는지 찾음
+      let asis = smallAlpha.indexOf(s[i]); // 몇번 인덱스에 있는지 체크
+      //소문자의 길이만큼으로 나눈 나머지는 결국 해당 알파벳의 인덱스가 됌.
+      let tobe = (asis + n + smallAlpha.length) % smallAlpha.length;
+      answer = answer + smallAlpha[tobe]; // 문자열에 해당 알파벳을 하나씩 추가
+      // 대문자일 경우도 마찬가지로 진행한다
+    } else {
+      let asis1 = bigAlpha.indexOf(s[i]);
+      let tobe1 = (asis1 + n + bigAlpha.length) % bigAlpha.length;
+      answer = answer + bigAlpha[tobe1];
+    }
+  }
+
+  return answer;
 }
